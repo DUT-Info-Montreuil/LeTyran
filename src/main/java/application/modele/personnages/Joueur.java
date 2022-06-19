3c83c9cb51ea493743cf3580a504c0bd91bbfd84
Steven <steven.yang.ys@gmail.com>
2022-06-19 22:42:12 +0200

    correction poserbloc

--- src/main/java/application/modele/personnages/Joueur.java
+++ src/main/java/application/modele/personnages/Joueur.java
@@ -91,6 +91,8 @@ public class Joueur extends Personnage {
             try {
                 if (x > (int) getX()/TUILE_TAILLE && Math.abs(x - getX()/TUILE_TAILLE) < 0.8)
                     x++;
+                if (y > (int) getY()/TUILE_TAILLE && Math.abs(y - getY()/TUILE_TAILLE) < 0.8)
+                    y++;
                 if (getEnv().getMinerai(x,y) == null) {
                     Materiau materiau;
                     switch (objetEquipe.getEntite().getClass().getSimpleName()) {
